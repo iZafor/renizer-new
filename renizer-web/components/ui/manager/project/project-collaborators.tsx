453558a051c584ab8b516a1d6f754f2d07a21196
iZafor <izafor65@gmail.com>
2024-06-17 19:31:09 +0600

    show unique contributors only

--- renizer-web/components/ui/manager/project/project-collaborators.tsx
+++ renizer-web/components/ui/manager/project/project-collaborators.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { ProjectCollaboration } from "@/lib/definitions";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { getInitial } from "@/lib/utils";
 import { PlusIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 interface ProjectCollaboratorsProps {
     collaborations: ProjectCollaboration[];
@@ -11,21 +14,29 @@ interface ProjectCollaboratorsProps {
 export default function ProjectCollaborators({
     collaborations,
 }: ProjectCollaboratorsProps) {
+    const [uniqueContributors, setUniqueContributors] = useState<string[]>([]);
+
+    useEffect(() => {
+        const collaboratorMap: { [key: string]: string } = {};
+        for (const co of collaborations) {
+            collaboratorMap[co.p_user_id] = co.contributor;
+        }
+        setUniqueContributors(Object.values(collaboratorMap));
+    }, [collaborations]);
+
     return (
         <div className="flex space-x-1 items-center">
             <div className="flex -space-x-3">
-                {collaborations.slice(0, 5).map((col, idx) => (
-                    <Avatar key={col.contributor + idx}>
+                {uniqueContributors.slice(0, 5).map((con, idx) => (
+                    <Avatar key={con + idx}>
                         <AvatarImage src="" alt="" />
-                        <AvatarFallback>
-                            {getInitial(col.contributor)}
-                        </AvatarFallback>
+                        <AvatarFallback>{getInitial(con)}</AvatarFallback>
                     </Avatar>
                 ))}
-                {collaborations.length > 5 && (
+                {uniqueContributors.length > 5 && (
                     <Avatar>
                         <AvatarFallback>
-                            +{collaborations.length - 5}
+                            +{uniqueContributors.length - 5}
                         </AvatarFallback>
                     </Avatar>
                 )}
